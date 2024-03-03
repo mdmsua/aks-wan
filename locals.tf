18eafc393437316a404c6ddd8707a1c42f74d57c
Dmytro Morozov <mdmsua@icloud.com>
2024-03-03 11:56:34 +0100

    Fix regional dns zones

--- locals.tf
+++ locals.tf
@@ -4,6 +4,6 @@ locals {
     version = var.spec.version
   }
   private_dns_zones          = yamldecode(file("${path.module}/files/private_dns_zones.yaml"))
-  regional_private_dns_zones = [for value in setproduct([for zone in local.private_dns_zones : zone if strcontains(zone, "*")], var.spec.locations) : replace("*", value[0], value[1])]
+  regional_private_dns_zones = [for value in setproduct([for zone in local.private_dns_zones : zone if strcontains(zone, "*")], var.spec.locations) : replace(value[0], "*", value[1])]
   all_private_dns_zones      = setunion(local.regional_private_dns_zones, [for zone in local.private_dns_zones : zone if !strcontains(zone, "*")])
 }
